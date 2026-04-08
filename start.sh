cebeb95016775f572f6ae5b4dbd2b0afda2f796f
Kalprajsinh <kalpraj51@gmail.com>
2026-04-08 23:51:25 +0530

    -

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Start Redis
 redis-server --bind 127.0.0.1 --protected-mode yes &
 
 sleep 1
 
-# Start FastAPI
-exec uvicorn app.server:app --host 0.0.0.0 --port ${PORT:-10000}
+exec python -m uvicorn app.server:app --host 0.0.0.0 --port ${PORT:-10000}
